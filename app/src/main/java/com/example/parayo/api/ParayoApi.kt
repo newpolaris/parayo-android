29794cd180d516a1ab394bf2d0e3bcac47248ecb
newpolaris <newpolaris@gmail.com>
2020-11-01 00:30:22 +0900

    Get hello texture from server via http

---
+++ app/src/main/java/com/example/parayo/api/ParayoApi.kt
@@ -0,0 +1,14 @@
+package com.example.parayo.api
+
+import com.example.parayo.api.response.ApiResponse
+import retrofit2.http.GET
+
+interface ParayoApi {
+    @GET("/api/v1/hello")
+    suspend fun hello(): ApiResponse<String>
+
+    companion object {
+        val instance = ApiGenerator()
+            .generate(ParayoApi::class.java)
+    }
+}
--- app/src/main/java/com/example/parayo/intro/IntroActivity.kt
+++ app/src/main/java/com/example/parayo/intro/IntroActivity.kt
@@ -1,13 +1,33 @@
 package com.example.parayo.intro
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import com.example.parayo.api.ParayoApi
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.anko.setContentView
 
 class IntroActivity : Activity() {
+    // TODO:
+    val hostAddress by lazy {
+        val packageName = applicationContext.packageName
+        val appInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+        appInfo.metaData.getString("HOST_ADDRESS")!!
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val ui = IntroActivityUI()
         ui.setContentView(this)
+
+        runBlocking {
+            try {
+                val response = ParayoApi.instance.hello()
+                Log.d("IntroActivity", response.data!!)
+            } catch (e: Exception) {
+                Log.e("IntroActivity", "Hello API 호출 오류", e)
+            }
+        }
     }
 }
